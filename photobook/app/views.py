3b8da7098610388d8b74e2dcaa4705fdabe44309
doginnn <diogenesemmanuel@gmail.com>
2021-03-23 20:11:52 -0300

    [UPD]Atualizando views com a home do projeto.

--- photobook/app/views.py
+++ photobook/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def home(request):
+    count = User.objects.count()
+    return render(request, 'base.html', {"count": count})
