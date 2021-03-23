0d9e427f936f06742b7605008371494e71c509fe
doginnn <diogenesemmanuel@gmail.com>
2021-03-23 20:28:26 -0300

    [UPD]Atualizando views para ficar tudo nas urls do projeto.

--- photobook/app/views.py
+++ photobook/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.views.generic import TemplateView
 
 
-def home(request):
-    count = User.objects.count()
-    return render(request, 'base.html', {"count": count})
+class Home(TemplateView):
+    template_name = 'home.html'
