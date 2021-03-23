be2ce4d0cffeca8ce1d3fdce91b72a7fc2d21bbb
doginnn <diogenesemmanuel@gmail.com>
2021-03-23 14:34:40 -0300

    [ADD]Criando modelo.

--- photobook/app/models.py
+++ photobook/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    name = models.CharField("Nome da Foto: ", max_length=100)
+    description = models.CharField("Descrição da Foto: ", max_length=100, null=True)
+    pub_date = models.DateField("Data de Publicação:")
+    status = models.BooleanField("Status")
+
+    def __str__(self):
+        return self.name
