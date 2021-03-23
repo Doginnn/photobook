c7fa0af0cdb737429b7a6007773cbdd1984589b8
doginnn <diogenesemmanuel@gmail.com>
2021-03-23 20:09:41 -0300

    [UPD]Atualizando settings.py

--- photobook/settings.py
+++ photobook/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'photobook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'photobook/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
