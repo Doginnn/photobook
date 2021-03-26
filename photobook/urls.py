ccdc7d8bae115218f5973b07136703e0fbe229ca
doginnn <diogenesemmanuel@gmail.com>
2021-03-25 22:20:06 -0300

    [UPD]Atualizando as rotas urls.py

--- photobook/urls.py
+++ photobook/urls.py
@@ -1,10 +1,14 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 from photobook.app import views
+from photobook.app.views import PhotoCreateView
 
 
 urlpatterns = [
     path('', views.base),
+    # path('PhotoCreateView', PhotoCreateView.as_view()),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
