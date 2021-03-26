fd0cdc00a90f3c1882818d09b8588e643796330a
doginnn <diogenesemmanuel@gmail.com>
2021-03-25 22:20:23 -0300

    [UPD]Atualizando as views.py

--- photobook/app/views.py
+++ photobook/app/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from photobook.app.models import Photo
 
 
 def base(request):
     form_base = User.objects.all()
-    return render(request, 'base.html', {'form_base': form_base})
+    # return render(request, 'base.html', {'form_base': form_base})
+    # return render(request, 'home.html', {'form_base': form_base})
+    return render(request, 'photo_form.html', {'form_base': form_base})
 
 
-def upload_photo(request):
-    pass
+class PhotoCreateView(CreateView):
+    model = Photo
+    fields = ['upload']
+    success_url = reverse_lazy('base')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        photos = Photo.objects.all()
+        context['photos'] = photos
+        return context
 
 
 def list_photo(request):
