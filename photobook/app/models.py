34136074238df88f0e4452b4f8742356cfed4677
doginnn <diogenesemmanuel@gmail.com>
2021-03-25 22:19:21 -0300

    [UPD]Atualizando models.py

--- photobook/app/models.py
+++ photobook/app/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Photo(models.Model):
-    image_url = models.ImageField("Imagem")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    upload = models.FileField()
     description = models.CharField("Descrição da Foto", max_length=100, blank=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Criada por")
-    pub_date = models.DateField("Data de Publicação")
 
     def __str__(self):
-        return self.name
+        return self.description
