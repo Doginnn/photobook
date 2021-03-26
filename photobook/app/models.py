cb8566571afcfd0131344758bc62412f87543b9e
doginnn <diogenesemmanuel@gmail.com>
2021-03-26 18:20:52 -0300

    [UPD]Atualizando upload = FileField >> ImageField

--- photobook/app/models.py
+++ photobook/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Photo(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    upload = models.FileField()
+    upload = models.ImageField()
     description = models.CharField("Descrição da Foto", max_length=100, blank=True)
 
     def __str__(self):
