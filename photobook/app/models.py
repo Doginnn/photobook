5bd84394c0d944de7dfb3757c5ce5a143409feb9
doginnn <diogenesemmanuel@gmail.com>
2021-03-29 14:35:27 -0300

    [UPD]Fazendo modificações no models.py

--- photobook/app/models.py
+++ photobook/app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Photo(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    upload = models.ImageField()
-    description = models.CharField("Descrição da Foto", max_length=100, blank=True)
+    upload = models.ImageField("Foto", null=True, blank=True)
+    description = models.CharField("Descrição da Foto", max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Selecione um Usuário")
 
     def __str__(self):
         return self.description
