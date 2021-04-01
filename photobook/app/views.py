cedad72caaebb2b86be3a0c425acc4ba275cbfb7
doginnn <diogenesemmanuel@gmail.com>
2021-03-31 21:06:30 -0300

    [RFT]Refatorando o APP para aprovar fotos.

--- photobook/app/views.py
+++ photobook/app/views.py
@@ -1,44 +1,40 @@
 from django.shortcuts import render, redirect
 from photobook.app.models import Photo
-from photobook.app.forms import PhotoForm
+from photobook.app.forms import PhotoForm, AprovedForm
+# from django.views.generic import
 
 
 def home(request):
     form_home = Photo.objects.all()
-    return render(request, 'base.html', {'form_home': form_home})
-
-
-def lista_foto(request):
-    form_lista_foto = Photo.objects.all()
-    return render(request, 'lista_foto.html', {'form_lista_foto': form_lista_foto})
-
-
-def fotos_aprovadas(request):
-    form_fotos_aprovadas = Photo.objects.all()
-    return render(request, 'fotos_aprovadas.html', {'form_fotos_aprovadas': form_fotos_aprovadas})
+    context = {'form_home': form_home}
+    return render(request, 'base.html', context)
 
 
 def upload_foto(request):
     if request.method == 'POST':
         form_upload_foto = PhotoForm(request.POST or None, request.FILES or None)
+        context = {'form_upload_foto': form_upload_foto}
         if form_upload_foto.is_valid():
             form_upload_foto.save()
-            return redirect('lista_foto')
+            return redirect('home')
     else:
         form_upload_foto = PhotoForm()
-    return render(request, 'upload_foto.html', {'form_upload_foto': form_upload_foto})
+        context = {'form_upload_foto': form_upload_foto}
+    return render(request, 'upload_foto.html', context)
 
 
-def aprova_foto(request, pk):
-    if request.method == 'POST':
-        order = Photo.objects.get(id=pk)
-        form_aprova_foto = PhotoForm(request.POST, instance=order)
-        if form_aprova_foto.is_valid():
-            form_aprova_foto.save()
-            return redirect('fotos_aprovadas')
-    else:
-        form_aprova_foto = PhotoForm()
-    return render(request, 'aprova_foto.html', {'form_aprova_foto': form_aprova_foto})
+# Função listando todas as fotos a serem aprovadas pelo ADMIN
+def lista_foto(request):
+    form_lista_foto = Photo.objects.all().order_by('-id')
+    context = {'form_lista_foto': form_lista_foto}
+    return render(request, 'lista_foto.html', context)
+
+
+# Será um Carousel na HOME, abaixo do Carousel TOP 3.
+def aprovar_fotos(request):
+    form_aprovar_fotos = Photo.objects.all().filter('id','aprovado')
+    context = {'form_aprovar_fotos': form_aprovar_fotos}
+    return render(request, 'aprovar_fotos.html', context)
 
 
 def deleta_foto(request, id):
@@ -47,14 +43,3 @@ def deleta_foto(request, id):
         foto.delete()
         return redirect('fotos_aprovadas')
     return render(request, 'deleta_foto.html', {'item': foto})
-
-# class PhotoCreateView(CreateView):
-#     model = Photo
-#     fields = ['upload']
-#     success_url = reverse_lazy('base')
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         photos = Photo.objects.all()
-#         context['photos'] = photos
-#         return context
