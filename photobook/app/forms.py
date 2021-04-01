ffc88f329bc8e41b4ccd00a2a38c3627b58ffc5d
doginnn <diogenesemmanuel@gmail.com>
2021-03-31 21:06:45 -0300

    [RFT]Refatorando o APP para aprovar fotos.

--- photobook/app/forms.py
+++ photobook/app/forms.py
@@ -5,4 +5,10 @@ from photobook.app.models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = '__all__'
+        fields = ['upload','description','user']
+
+
+class AprovedForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['aprovado']
